b861df245dca5d393c15a89bf357907ee4017da6
Karan Thukral <karanthukral@gmail.com>
2013-02-26 12:33:55 -0500

    Added Zerver Backend and fetching articles from the verge

--- web/js/main.js
+++ web/js/main.js
@@ -1,14 +1,22 @@
-(function (window, document, $, cards, App, MyAPI) {
+			
+			App.populator('articleList',function(page){
 
-	App.populator('page2', function (page) {
-		$(page).find('.app-content .app-button')
-			.click(function () {
-				MyAPI.ping('ping from client', function (str) {
-					alert(str);
+				MyAPI.getArticles(function(meta,articles){
+
+					populateArticleList(articles);
+
 				});
-			});
+
+			
+
+			function populateArticleList(data){
+				console.log(data);
+			}
+
 	});
-
-	App.load('home');
-
-})(window, document, Zepto, cards, App, MyAPI);
+			try{
+				App.restore();
+			}
+			catch (err) {
+				App.load('articleList');
+			}
--- web/zerver/MyAPI.js
+++ web/zerver/MyAPI.js
@@ -1,4 +1,5 @@
-exports.ping = function (str, callback) {
-	console.log(str);
-	callback('ping from server');
-};
+var feedparser = require('../../node_modules/feedparser');
+
+exports.getArticles = function (callback) {
+	feedparser.parseUrl('http://www.theverge.com/rss/index.xml').on('complete',callback);
+}
